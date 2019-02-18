41a97b02f95954e2529f012e569ec2282f776f66
Asger Meldgaard <35038126+asgerMe@users.noreply.github.com>
2019-02-18 15:16:43 -0500

    Delete deep_earth.py

--- deep_earth.py
+++
@@ -1,6 +0,0 @@
-import sys
-import getopt
-import numpy as np
-
-diff_kernel = [[np.asarray([[[0, 1, 0], [1, 1, 1], [0, 1, 0]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[0, 1, 0], [1, 1, 1], [0, 1, 0]]])]]
-print(np.shape(diff_kernel))
